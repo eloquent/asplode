9780c5478f7521115affb370fe8e7ba12105a810
Erin Millard <ezzatron@gmail.com>
2013-06-07 14:30:40 +1000

    Trying again for 5.3.

--- test/suite/Eloquent/Asplode/FunctionalTest.php
+++ test/suite/Eloquent/Asplode/FunctionalTest.php
@@ -31,8 +31,7 @@ class FunctionalTest extends PHPUnit_Framework_TestCase
      */
     public function testOneLineInstallation()
     {
-        var_dump(version_compare(PHP_VERSION, '5.4.0'));ob_flush();
-        if (!version_compare(PHP_VERSION, '5.4.0')) {
+        if (version_compare(PHP_VERSION, '5.4.0') < 0) {
             $this->markTestSkipped('Requires PHP >= 5.4');
 
             return;
